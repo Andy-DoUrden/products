7bcb45a2bafb7c0f33282af8680154ba33bc9e62
Andy-DoUrden <111894438+Andy-DoUrden@users.noreply.github.com>
2023-01-16 20:20:14 +0200

    Build-1

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+(() => {
+  const refs = {
+    openModalBtn1: document.querySelector("[data-modal-open-1]"),
+    openModalBtn2: document.querySelector("[data-modal-open-2]"),
+    openModalBtn3: document.querySelector("[data-modal-open-3]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn1.addEventListener("click", toggleModal);
+  refs.openModalBtn2.addEventListener("click", toggleModal);
+  refs.openModalBtn3.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
